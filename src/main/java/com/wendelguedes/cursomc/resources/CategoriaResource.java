9d526db18d1f620b8c553238b7d1253b71623627
wendelguedes <wendelguedes@gmail.com>
2019-01-29 23:05:52 -0200

    Configuracao do banco h2 e add jpa

--- src/main/java/com/wendelguedes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wendelguedes/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.wendelguedes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wendelguedes.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Automóveis");
+		Categoria c2 = new Categoria(2, "Eletrônicos");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(c1);
+		categorias.add(c2);
+
+		return categorias;
 	}
 
 }
